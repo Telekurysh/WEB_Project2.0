29fdc605e997d4f6934ed4ee0511e0fec59d0e6c
Misha7128 <mic.mamaev@yandex.ru>
2020-05-12 19:17:35 +0300

    first

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = 'NzA5NDUyOTQ5MTc2NDUxMTQz.XrmskQ.moGvGr3bC0b01sr3RJUgjRepb7g'  # bot token
+POST_ID = 709459064567890087  # post id to read reaction from
+ROLES = {
+    '🎮': 709456391651459074,  # gamer
+    '💻': 709456487780450334,  # programmer
+    '🤼': 709456526569504810,  # sport
+    '😆': 709456571624718336,  # smile
+}
+EXCROLES = ()
+
+MAX_ROLES_PER_USER = 4
